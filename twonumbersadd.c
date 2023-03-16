e817a16d6dd37ac67a76bda44bfb0d64bbd18f39
Rahul A M <91199828+rahulam10@users.noreply.github.com>
2023-03-15 21:38:29 -0700

    Add files via upload

---
+++ twonumbersadd.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int num1, num2, result;
+    printf("Enter two number: \n ");
+    scanf("%d%d", &num1, &num2);
+    result = num1 + num2;
+    printf("Result: %d\n",result);
+    return 0;
+}
